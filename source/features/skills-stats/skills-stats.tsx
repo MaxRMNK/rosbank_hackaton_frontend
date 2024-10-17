a1c8321f6ec802e1b6d0584ccd5815e6fb03b7f6
MaxRMNK <115977436+MaxRMNK@users.noreply.github.com>
2024-10-18 02:29:14 +0500

    fix: SkillsStats deleting comments

--- source/features/skills-stats/skills-stats.tsx
+++ source/features/skills-stats/skills-stats.tsx
@@ -1,64 +1,10 @@
-'use client';
-
-import React, { useState } from 'react';
 import cn from 'classnames';
 import classes from './styles.module.scss';
-import { typeSkillsStatsProps, typeSkills } from './types';
+import { typeSkillsStatsProps } from './types';
 import { SkillTableRow } from '../skill-table-row';
-// import { TeamTableRow } from '../team-table-row';
 
 export const SkillsStats: React.FC<typeSkillsStatsProps> = props => {
   const { skillData, className } = props;
-
-  // Для сортировки по количеству человек в команде
-  // const [isPeople, setIsPeople] = useState<number>(0);
-
-  //---------------------
-
-  // interface Level {
-  //   none: number;
-  //   beginner: number;
-  //   intermediate: number;
-  //   advanced: number;
-  //   expert: number;
-  // }
-
-  // interface ObjectWithLevels {
-  //   level: Level;
-  //   targetLevel: Level;
-  // }
-
-  // const data: ObjectWithLevels = {
-  //   level: {
-  //     none: 0,
-  //     beginner: 1,
-  //     intermediate: 1,
-  //     advanced: 2,
-  //     expert: 1,
-  //   },
-  //   targetLevel: {
-  //     none: 0,
-  //     beginner: 0,
-  //     intermediate: 1,
-  //     advanced: 2,
-  //     expert: 2,
-  //   },
-  // };
-
-  // const calculateDifference = (obj1: Level, obj2: Level) => {
-  //   const difference: Level = {
-  //     none: obj1.none - obj2.none,
-  //     beginner: obj1.beginner - obj2.beginner,
-  //     intermediate: obj1.intermediate - obj2.intermediate,
-  //     advanced: obj1.advanced - obj2.advanced,
-  //     expert: obj1.expert - obj2.expert,
-  //   };
-  //   return difference;
-  // };
-
-  // const difference = calculateDifference(data.targetLevel, data.level);
-
-  // console.log('ffff', difference);
 
   return (
     <div className={cn(className, classes.skills)}>
@@ -94,7 +40,6 @@ export const SkillsStats: React.FC<typeSkillsStatsProps> = props => {
               type={item.type}
               level={item.level}
               targetLevel={item.targetLevel}
-              // setIsPeople={setIsPeople}
               className={cn(classes.tableSkillsRow)}
             />
           );
@@ -103,69 +48,3 @@ export const SkillsStats: React.FC<typeSkillsStatsProps> = props => {
     </div>
   );
 };
-
-// const [checked, setChecked] = React.useState(false);
-
-// const chengeCheckbox = (e: any) => {
-//   setChecked(!checked);
-//   // console.log(e?.target?.name);
-// };
-
-// const requestForTraining = (e: any) => {
-//   console.log('Отправить запрос на обучение', e);
-// };
-
-//----------------------------------
-
-// Работает
-
-//----------------------------------
-
-// interface Levels {
-//   [none: string]: number;
-//   beginner: number;
-//   intermediate: number;
-//   advanced: number;
-//   expert: number;
-// }
-
-// interface LevelDifference {
-//   [key: string]: number;
-// }
-
-// const level = {
-//   none: 0,
-//   beginner: 1,
-//   intermediate: 1,
-//   advanced: 2,
-//   expert: 1,
-// };
-
-// const targetLevel = {
-//   none: 0,
-//   beginner: 0,
-//   intermediate: 1,
-//   advanced: 2,
-//   expert: 2,
-// };
-
-// const calculateDifference = (
-//   levelA: Levels,
-//   levelB: Levels,
-// ): LevelDifference => {
-//   const difference: LevelDifference = {};
-
-//   for (const key in levelA) {
-//     if (levelA.hasOwnProperty(key) && levelB.hasOwnProperty(key)) {
-//       difference[key] = levelA[key] - levelB[key];
-//     }
-//   }
-
-//   return difference;
-// };
-
-// const difference = calculateDifference(level, targetLevel);
-
-// console.log('ffff', difference);
-
-//----------------------------------
